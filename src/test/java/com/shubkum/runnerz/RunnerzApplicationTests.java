ef33fcfaa5be88185b34112158bdfab4b7f1da4b
shubham kumar <shubham.kumar0109@gmail.com>
2024-03-20 10:17:39 +0530

    first commit

---
+++ src/test/java/com/shubkum/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubkum.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
